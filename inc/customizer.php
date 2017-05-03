8a72f635d761ec5e768dfbcac329f4960eade7af
Brad Williams <braginteractive@gmail.com>
2017-05-03 11:31:45 -0500

    remove unused customer.js call

--- inc/customizer.php
+++ inc/customizer.php
@@ -16,11 +16,3 @@ function strappress_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 }
 add_action( 'customize_register', 'strappress_customize_register' );
-
-/**
- * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
- */
-function strappress_customize_preview_js() {
-	wp_enqueue_script( 'strappress_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
-}
-add_action( 'customize_preview_init', 'strappress_customize_preview_js' );
