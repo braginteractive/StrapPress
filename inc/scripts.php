150cd6b02b023169889439e56ad471272131b3f5
Brad Williams <braginteractive@gmail.com>
2017-05-15 12:31:12 -0500

    use minified styles

--- inc/scripts.php
+++ inc/scripts.php
@@ -3,7 +3,7 @@
  * Enqueue scripts and styles.
  */
 function strappress_scripts() {
-	wp_enqueue_style( 'strappress-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'strappress-style', get_stylesheet_directory_uri() . '/style.min.css', array(), '1.0.0' );
 
 	wp_enqueue_script( 'strappress-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), ' ', true );
 
