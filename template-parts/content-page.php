935df64d92c008736ebde9dbebe3d7fbc8af18ca
Brad Williams <braginteractive@gmail.com>
2017-12-15 11:04:40 -0600

    add thumbnails to page and search results

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -10,6 +10,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="post-thumbnail">
+			<?php the_post_thumbnail('full', array('class' => 'rounded')); ?>
+		</div><!--  .post-thumbnail -->
+	<?php endif; ?>
+
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,6 +10,15 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
+	<?php if ( has_post_thumbnail() ) : ?>
+		<div class="post-thumbnail">
+		    <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+		        <?php the_post_thumbnail('full', array('class' => 'rounded')); ?>
+		    </a>
+		</div><!--  .post-thumbnail -->
+	<?php endif; ?>
+
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 
