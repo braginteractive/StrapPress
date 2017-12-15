c0509bf1d94262930a088ae1030420c935cb786b
Brad Williams <braginteractive@gmail.com>
2017-12-15 10:49:21 -0600

    add Font Awesome 5

--- inc/scripts.php
+++ inc/scripts.php
@@ -7,8 +7,26 @@ function strappress_scripts() {
 
 	wp_enqueue_script( 'strappress-js', get_template_directory_uri() . '/js/dist/scripts.min.js', array('jquery'), ' ', true );
 
+	wp_enqueue_script( 'strappress-fa', '//use.fontawesome.com/releases/v5.0.1/js/all.js', array(), '5.0.1' );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'strappress_scripts' );
+
+
+/**
+ * Filter the HTML script tag of `strappress-fa` script to add `defer` attribute.
+ *
+ * @param string $tag    The <script> tag for the enqueued script.
+ * @param string $handle The script's registered handle.
+ *
+ * @return   Filtered HTML script tag.
+ */
+function strappress__defer_attribute( $tag, $handle ) {
+    if ( 'strappress-fa' === $handle ) {
+        return str_replace( ' src', ' defer src', $tag );
+    }
+}
+add_filter( 'script_loader_tag', 'strappress__defer_attribute', 10, 2 );
