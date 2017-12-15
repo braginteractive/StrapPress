e6a98879d98721e587ec0268d4a9f58f8553cec9
Brad Williams <braginteractive@gmail.com>
2017-12-15 11:01:39 -0600

    added icons to post meta

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -34,20 +34,20 @@ function strappress_posted_on() {
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span>
-	<span class="byline">' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="posted-on"><i class="far fa-clock"></i>' . $posted_on . '</span>
+	<span class="byline"><i class="far fa-user"></i>' . $byline . '</span>'; // WPCS: XSS OK.
 
 	// Hide category and tag text for pages.
 	if ( 'post' === get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
 		$categories_list = get_the_category_list( esc_html__( ', ', 'strappress' ) );
 		if ( $categories_list && strappress_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( '%1$s', 'strappress' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+			printf( '<span class="cat-links"><i class="far fa-folder-open"></i>' . esc_html__( '%1$s', 'strappress' ) . '</span>', $categories_list ); // WPCS: XSS OK.
 		}
 	}
 
 	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
+		echo '<span class="comments-link"><i class="far fa-comments"></i>';
 		/* translators: %s: post title */
 		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'strappress' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
 		echo '</span>';
@@ -62,11 +62,11 @@ if ( ! function_exists( 'strappress_entry_footer' ) ) :
  */
 function strappress_entry_footer() {
 	// Hide category and tag text for pages.
-	if ( 'post' === get_post_type() ) {
+	if ( 'post' === get_post_type() && is_single() ) {
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'strappress' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( '%1$s', 'strappress' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+			printf( '<span class="tags-links"><i class="fas fa-tags"></i>' . esc_html__( '%1$s', 'strappress' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
 	}
 
